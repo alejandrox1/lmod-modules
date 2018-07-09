1444570ca0546194bf316082acb06fb35b26573c
alejandrox1 <alarcj137@gmail.com>
2018-07-09 16:42:01 -0500

    tophat/ls5_tophat_test-nomod.sh test tophat installation in ls5
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

---
+++ tophat/ls5_tophat_test-nomod.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#SBATCH -A my_project
+#SBATCH -p normal
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 10:00
+#SBATCH -J tophat_test
+#SBATCH -o slurm.%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alarcj137@gmail.com
+set -e                                                                          
+set -o pipefail                                                                 
+set -o posix
+module load remora
+module load boost/1.59                                                          
+module load perl/5.22.1                                                         
+module load bowtie/2.2.6
+
+export PATH=$PATH:$WORK/apps/tophat/v2.1.1/bin
+
+export APPS_SRC="${WORK}/apps"
+export TOPHAT_TEST="${APPS_SRC}/tophat-test"
+
+mkdir -p "${TOPHAT_TEST}" && \
+    wget --no-check-certificate https://ccb.jhu.edu/software/tophat/downloads/test_data.tar.gz && \
+    tar -xvzf test_data.tar.gz && \
+    cd test_data && \
+    remora tophat -r 20 test_ref reads_1.fq reads_2.fq
